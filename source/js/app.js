322beeb318b3b843f9626d175fc87093c9b623b2
VasiliuKr <vasil.krasnov@gmail.com>
2018-03-13 17:41:03 +0300

    update

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 	$('.slider-main').slick({
 		dots: true,
+		arrows: false,
 		autoplay: false,
 		autoplaySpeed: 3000,
 		adaptiveHeight: true,
 		asNavFor: '.slider-text'
 	});
 	$('.slider-text').slick({
-		asNavFor: '.slider-main'
+		asNavFor: '.slider-main',
+		arrows: false
+	});
+	$('.top-products__list').slick({
+		slidesToShow: 2
+	});
+	$('.slider-collections').slick({
+
+	});
+	$('.social-gallery__slider').slick({
+		slidesToShow: 3
 	});
 });
